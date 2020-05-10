88f912cc811179a1dcaa1c7e7c6a8eac52d9090a
Christina Li <cli50@stevens.edu>
2020-05-09 20:41:02 -0700

    basis of my website, home page almost complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Footer from './components/Footer';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='bg-dark text-white' style={{ height: '100vh' }}>
+        <Nav />
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
